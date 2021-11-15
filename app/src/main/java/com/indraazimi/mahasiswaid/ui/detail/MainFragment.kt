9b26c69f41bc0745bf20f8d46ae4b7665033274e
Indra Azimi <indraazimi@yahoo.com>
2021-11-15 22:40:58 +0700

    Membuat navigasi dengan Navigation Component

--- app/src/main/java/com/indraazimi/mahasiswaid/ui/detail/MainFragment.kt
+++ app/src/main/java/com/indraazimi/mahasiswaid/ui/detail/MainFragment.kt
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ActionMode
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.indraazimi.mahasiswaid.R
@@ -94,7 +95,7 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.fab.setOnClickListener {
-            MainDialog().show(requireActivity().supportFragmentManager, "MainDialog")
+            findNavController().navigate(R.id.action_mainFragment_to_mainDialog)
         }
 
         myAdapter = MainAdapter(handler)
